77992d877874912a472fa2f684b3214fe3fd8fbb
Jackei94 <55141365+Jackei94@users.noreply.github.com>
2021-10-29 12:43:47 +0200

    Updated Material Module

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -3,6 +3,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from "@angular/material/select";
+import { MatOptionModule } from "@angular/material/core";
 import { CommonModule } from "@angular/common";
 
 @NgModule({
@@ -13,7 +15,9 @@ import { CommonModule } from "@angular/common";
         MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSelectModule,
+        MatOptionModule
     ],
     providers: []
   })
