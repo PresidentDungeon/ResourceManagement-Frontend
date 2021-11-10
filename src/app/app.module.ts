26de4a317fb1406f3fd1122376b03f19b428e8b6
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-11-10 22:39:17 +0100

    Custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from "./shared/modules/shared.module";
 import { MaterialModule } from './shared/modules/material.module';
+import { SocketIoModule } from "ngx-socket-io";
 
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
@@ -14,12 +15,12 @@ import { VerificationComponent } from './verification/verification.component';
 import { HiringpageComponent } from './hiringpage/hiringpage.component';
 import { RegisterComponent } from './register/register.component';
 import { VerificationLinkComponent } from './verification-link/verification-link.component';
-
-import { AuthInterceptor } from "./auth-guards/auth.interceptor";
 import { VerificationCardComponent } from './verification-card/verification-card.component';
 import { UserlistComponent } from "./userlist/userlist.component";
 import { ProfilepageComponent } from './profilepage/profilepage.component';
-import { SocketIoModule } from "ngx-socket-io";
+
+import { AuthInterceptor } from "./auth-guards/auth.interceptor";
+import { ReplaceNullWithTextPipe } from './shared/helpers/replace-null-with-text.pipe';
 
 @NgModule({
   declarations: [
@@ -33,17 +34,18 @@ import { SocketIoModule } from "ngx-socket-io";
     VerificationLinkComponent,
     VerificationCardComponent,
     UserlistComponent,
-    ProfilepageComponent
+    ProfilepageComponent,
+    ReplaceNullWithTextPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    SharedModule,
-    MaterialModule,
-    SocketIoModule.forRoot({url: ''})
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        SharedModule,
+        MaterialModule,
+        SocketIoModule.forRoot({url: ''})
+    ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
